93dd29ee9fe3486f7fa966ca597c62b3395508b7
shinyeongwoon <duddns119@naver.com>
2023-08-29 17:47:36 +0900

    init commit

---
+++ Back/src/main/java/team/travelmate/TravelmateApplication.java
@@ -0,0 +1,13 @@
+package team.travelmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelmateApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/team/travelmate/TravelmateApplicationTests.java
@@ -0,0 +1,13 @@
+package team.travelmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
